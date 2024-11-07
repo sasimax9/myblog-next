994c4903cafbcedc6e95dee4ff934f6d24e0acb0
Suma2310 <sumasrikada@gmail.com>
2024-11-08 00:25:52 +0530

    next template

---
+++ pages/about/index.tsx
@@ -0,0 +1,47 @@
+import { title } from "@/components/primitives";
+import DefaultLayout from "@/layouts/default";
+import { Card, CardBody, CardFooter, CardHeader } from "@nextui-org/card";
+import { Divider } from "@nextui-org/divider";
+import { Link } from "@nextui-org/link";
+
+
+export default function DocsPage() {
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="inline-block max-w-lg text-center justify-center">
+          <Card className="max-w-[400px]">
+            <CardHeader className="flex gap-3">
+              {/* <Image
+                alt="nextui logo"
+                height={40}
+                radius="sm"
+                src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+                width={40}
+              /> */}
+              <div className="flex flex-col">
+                <p className="text-md">NextUI</p>
+                <p className="text-small text-default-500">nextui.org</p>
+              </div>
+            </CardHeader>
+            <Divider />
+            <CardBody>
+              <p>Make beautiful websites regardless of your design experience.</p>
+            </CardBody>
+            <Divider />
+            <CardFooter>
+              <Link
+                isExternal
+                showAnchorIcon
+                href="https://github.com/nextui-org/nextui"
+              >
+                Visit source code on GitHub.
+              </Link>
+            </CardFooter>
+          </Card>
+          {/* <h1 className={title()}></h1> */}
+        </div>
+      </section>
+    </DefaultLayout>
+  );
+}
